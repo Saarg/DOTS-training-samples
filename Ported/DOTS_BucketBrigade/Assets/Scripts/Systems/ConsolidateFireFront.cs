c7de840a93bd0c51390ead4d578efad6599d781d
tim <tim@tim.tim>
2020-03-03 15:14:19 -0500

    Add the consolidate fire front system
     - untested

---
+++ Ported/DOTS_BucketBrigade/Assets/Scripts/Systems/ConsolidateFireFront.cs
@@ -0,0 +1,52 @@
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Mathematics;
+
+//[UpdateAfter()]
+public class ConsolidateFireFront : JobComponentSystem
+{
+    private EndSimulationEntityCommandBufferSystem m_CommandBufferSystem;
+    
+    protected override void OnCreate()
+    {
+        m_CommandBufferSystem = World
+            .DefaultGameObjectInjectionWorld
+            .GetOrCreateSystem<EndSimulationEntityCommandBufferSystem>();
+    }
+    protected override JobHandle OnUpdate(JobHandle inputDeps)
+    {
+        EntityCommandBuffer.Concurrent ecb = m_CommandBufferSystem.CreateCommandBuffer().ToConcurrent();
+        var grid = GetSingleton<Grid>();
+        var handle = Entities.ForEach((Entity entity, int entityInQueryIndex, FireTag tag, PositionInGrid posInGrid) =>
+            {
+                var aroundPos = new int2[]
+                {
+                    posInGrid.Value + new int2(-1, -1),
+                    posInGrid.Value + new int2(-1, 0),
+                    posInGrid.Value + new int2(-1, 1),
+                    posInGrid.Value + new int2(0, -1),
+                    //posInGrid.Value + new int2(0, 0),
+                    posInGrid.Value + new int2(0, 1),
+                    posInGrid.Value + new int2(1, -1),
+                    posInGrid.Value + new int2(1, 0),
+                    posInGrid.Value + new int2(1, 1),
+                };
+
+                var isInFront = true;
+                foreach (var pos in aroundPos)
+                {
+                    if (grid.Value.ContainsKey(pos))
+                        isInFront = false;
+                }
+                
+                if (!isInFront)
+                    ecb.RemoveComponent<FireTag>(entityInQueryIndex, entity);
+                
+            })
+            .Schedule(inputDeps);
+        
+        m_CommandBufferSystem.AddJobHandleForProducer(handle);
+        
+        return handle;
+    }
+}
