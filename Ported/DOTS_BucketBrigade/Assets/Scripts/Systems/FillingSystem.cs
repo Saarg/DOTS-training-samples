00b84fd19a752ca8669c1988b4cff9c567072c66
saarg <jean.milsonneau@lavabit.com>
2020-03-03 15:27:40 -0500

    sorry

--- Ported/DOTS_BucketBrigade/Assets/Scripts/Systems/FillingSystem.cs
+++ Ported/DOTS_BucketBrigade/Assets/Scripts/Systems/FillingSystem.cs
@@ -5,10 +5,12 @@ public class FillingSystem : SystemBase
 {
     protected override void OnUpdate()
     {
+        var deltaTime = Time.DeltaTime;
+        
         Entities.WithChangeFilter<FillRate>().WithNone<BucketTag>().ForEach((ref GradientState gradientState, 
             in FillRate fillState, in Capacity capacity) =>
         {
-            gradientState.Value = math.min(capacity.Value, gradientState.Value + fillState.Value * Time.DeltaTime);
+            gradientState.Value = math.min(capacity.Value, gradientState.Value + fillState.Value * deltaTime);
         }).Schedule();
     }
 }
