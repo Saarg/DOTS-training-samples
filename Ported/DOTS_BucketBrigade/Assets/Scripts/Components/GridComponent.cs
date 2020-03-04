65e6ef9ecde2378aed0ea79454e95bab5aa56d34
saarg <jean.milsonneau@lavabit.com>
2020-03-04 13:57:53 -0500

    Update grid component

--- Ported/DOTS_BucketBrigade/Assets/Scripts/Components/GridComponent.cs
+++ Ported/DOTS_BucketBrigade/Assets/Scripts/Components/GridComponent.cs
@@ -1,9 +1,11 @@
-﻿using Unity.Collections.LowLevel.Unsafe;
+﻿using System;
+using Unity.Collections.LowLevel.Unsafe;
 using Unity.Entities;
 using Unity.Mathematics;
 
 public struct Grid : IComponentData
 {
+    [Serializable]
     public struct Cell
     {
         public enum ContentFlags
@@ -19,4 +21,17 @@ public struct Grid : IComponentData
     public UnsafeHashMap<int2, Cell> Physical;
     public UnsafeHashMap<int2, int /*<unused>*/> Simulation;
 	public float CellSize;
+
+    int2 ToGridPos(Position2D position2D) => ToGridPos(position2D.Value);
+    int2 ToGridPos(float3 position) => ToGridPos(position.xz);
+    int2 ToGridPos(float2 position2D)
+    {
+        return (int2) math.floor(position2D / CellSize);
+    }
+
+    float2 ToPos2D(PositionInGrid gridPos) => ToPos2D(gridPos.Value);
+    float2 ToPos2D(int2 gridPos)
+    {
+        return (float2) (gridPos) * CellSize;
+    }
 }
