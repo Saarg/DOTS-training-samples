40a771efc6996a290f38a41cab3041958ebcdfe4
saarg <jean.milsonneau@lavabit.com>
2020-03-04 11:47:18 -0500

    Water filling system

--- Ported/DOTS_BucketBrigade/Assets/Scripts/AuthoringComponents/WaterAuthoring.cs
+++ Ported/DOTS_BucketBrigade/Assets/Scripts/AuthoringComponents/WaterAuthoring.cs
@@ -6,5 +6,7 @@ public class WaterAuthoring : MonoBehaviour, IConvertGameObjectToEntity
     public void Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem)
     {
         dstManager.AddComponent<WaterTag>(entity);
+        dstManager.AddComponentData(entity, new GradientState { Value = 0.9f });
+        dstManager.AddComponentData(entity, new Capacity { Value = transform.localScale.x * 10 });
     }
 }
---
+++ Ported/DOTS_BucketBrigade/Assets/Scripts/Systems/WaterFillingSystem.cs
@@ -0,0 +1,22 @@
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+
+public class WaterFillingSystem : SystemBase
+{
+    protected override void OnUpdate()
+    {
+        var deltaTime = Time.DeltaTime;
+
+        var waterSingleton = GetSingleton<WaterMaster>();
+        Entities.WithAll<WaterTag>().ForEach((ref GradientState gradientState, ref NonUniformScale scale, 
+            in Capacity capacity) =>
+        {
+            gradientState.Value = math.min(1.0f, 
+                gradientState.Value + waterSingleton.RefillRate * deltaTime / capacity.Value);
+
+            scale.Value.x = gradientState.Value * capacity.Value * 0.1f;
+            scale.Value.z = gradientState.Value * capacity.Value * 0.1f;
+        }).Schedule();
+    }
+}
