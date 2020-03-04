8a47f06193feb6257cad1ad46f4b99ec836c7ac8
saarg <jean.milsonneau@lavabit.com>
2020-03-04 13:36:12 -0500

    Improve GridUpdate perf

--- Ported/DOTS_BucketBrigade/Assets/Scripts/Systems/GridUpdate.cs
+++ Ported/DOTS_BucketBrigade/Assets/Scripts/Systems/GridUpdate.cs
@@ -1,3 +1,4 @@
+using Unity.Burst;
 using Unity.Collections;
 using Unity.Collections.LowLevel.Unsafe;
 using Unity.Entities;
@@ -7,24 +8,68 @@ using Unity.Mathematics;
 [UpdateInGroup(typeof(InitializationSystemGroup))]
 public class GridUpdate : JobComponentSystem
 {
+    private EntityCommandBufferSystem m_CommandBufferSystem;
+
+    [BurstCompile, RequireComponentTag(typeof(ToDeleteFromGridTag))]
+    struct ClearFiresGrid : IJobForEachWithEntity<PositionInGrid>
+    {
+        [NativeSetThreadIndex]
+        public int ThreadIndex;
+        
+        public UnsafeHashMap<int2, Grid.Cell> Grid;
+        public EntityCommandBuffer.Concurrent CommandBuffer;
+        
+        public void Execute(Entity entity, int index, ref PositionInGrid positionInGrid)
+        {
+            Grid.Remove(positionInGrid.Value);
+            CommandBuffer.DestroyEntity(ThreadIndex, entity);
+        }
+    }
+
+    [BurstCompile, RequireComponentTag(typeof(NewFireTag))]
+    struct NewFireJob : IJobForEachWithEntity<PositionInGrid>
+    {
+        public UnsafeHashMap<int2, int> Grid;
+        
+        public void Execute(Entity entity, int index, ref PositionInGrid positionInGrid)
+        {
+            Grid.Remove(positionInGrid.Value);
+        }
+    }
+    
+    protected override void OnCreate()
+    {
+        m_CommandBufferSystem = World.GetOrCreateSystem<EndInitializationEntityCommandBufferSystem>();
+    }
+
     protected override JobHandle OnUpdate(JobHandle inputDeps)
     {
         var grid = GetSingleton<Grid>();
 
-        grid.Physical.Capacity = math.max(grid.Physical.Capacity, grid.Physical.Length + grid.Simulation.Length);
-        grid.Simulation.Capacity = math.max(grid.Simulation.Capacity, grid.Simulation.Length * 2);
-
-        Entities.WithStructuralChanges().ForEach((Entity entity, ToDeleteFromGridTag tag, PositionInGrid pos) =>
+        if (grid.Physical.Capacity < grid.Physical.Length + grid.Simulation.Length)
         {
-            grid.Physical.Remove(pos.Value);
-            EntityManager.DestroyEntity(entity);
-        }).Run();
+            grid.Physical.Capacity = (grid.Physical.Length + grid.Simulation.Length) * 2;
+        }
+
+        if (grid.Simulation.Capacity < grid.Simulation.Length * 2)
+        {
+            grid.Simulation.Capacity = grid.Simulation.Length * 4;
+        }
+
+        var clearGridJob = new ClearFiresGrid
+        {
+            Grid = grid.Physical,
+            CommandBuffer = m_CommandBufferSystem.CreateCommandBuffer().ToConcurrent()
+        };
+        var clearGridJobHandle = clearGridJob.ScheduleSingle(this, inputDeps);
+        m_CommandBufferSystem.AddJobHandleForProducer(clearGridJobHandle);
         
-        Entities.WithStructuralChanges().ForEach((Entity entity, NewFireTag tag, PositionInGrid pos) =>
+        var newFireJob = new NewFireJob
         {
-            grid.Simulation.Remove(pos.Value);
-        }).Run();
+            Grid = grid.Simulation
+        };
+        var newFireJobHandle = newFireJob.ScheduleSingle(this, clearGridJobHandle);
 
-        return inputDeps;
+        return newFireJobHandle;
     }
 }
