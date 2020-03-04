6fe89dcc311281160e4977e1daa57ffc46da05ab
tim <tim@tim.tim>
2020-03-04 11:04:52 -0500

    Add positionInGrid to Fire

--- Ported/DOTS_BucketBrigade/Assets/Scripts/AuthoringComponents/FireAuthoring.cs
+++ Ported/DOTS_BucketBrigade/Assets/Scripts/AuthoringComponents/FireAuthoring.cs
@@ -6,5 +6,6 @@ public class FireAuthoring : MonoBehaviour, IConvertGameObjectToEntity
     public void Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem)
     {
         dstManager.AddComponent<FireTag>(entity);
+        dstManager.AddComponent<PositionInGrid>(entity);
     }
 }
