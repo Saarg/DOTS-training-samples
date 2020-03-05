30f2b111fee434929b49c82601ec93f26611fc22
Alexandre Forget <forget.lexandre@gmail.com>
2020-03-05 13:54:38 -0500

    Burst Compile Targeting System

--- Ported/DOTS_BucketBrigade/Assets/Scripts/Systems/TargetingSystem.cs
+++ Ported/DOTS_BucketBrigade/Assets/Scripts/Systems/TargetingSystem.cs
@@ -1,71 +1,92 @@
-﻿using Unity.Entities;
+﻿using Unity.Burst;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Jobs;
 using Unity.Mathematics;
 
-public class TargetingSystem : SystemBase
+public class TargetingSystem : JobComponentSystem
 {
-
-    protected override void OnUpdate()
+    [BurstCompile]
+    struct FromToJob : IJobForEach<FromTo>
     {
-        var position2Ds = GetComponentDataFromEntity<Position2D>(true);
-        Entities.ForEach((Entity entity, ref FromTo fromTo) =>
-        {
-            if (fromTo.RelativeTo == Entity.Null) 
-                return;
-            var position = position2Ds[fromTo.RelativeTo];
+        [ReadOnly]
+        public Grid Grid;
+        
+        [ReadOnly]
+        public GameMaster GameMaster;
+        
+        [ReadOnly]
+        public ComponentDataFromEntity<Position2D> Position2DFromEntity;
 
-            var nearestFire = FindNearestOfTag(position.Value, Grid.Cell.ContentFlags.Fire);
-            var nearestWater = FindNearestOfTag(nearestFire, Grid.Cell.ContentFlags.Water);
-            
+        float2 FindNearestOfTag(float2 position, Grid.Cell.ContentFlags flag, Grid grid, GameMaster gameMaster)
+        {
+            var biggerLength = gameMaster.NbRows > gameMaster.NbCols ? gameMaster.NbRows : gameMaster.NbCols;
+
+            for (var i = 0; i < biggerLength; ++i)
+            {
+                for (var j = 0; j <= i; ++j)
+                {
+                    for (var k = 0; k <= i; ++k)
+                    {
+                        if (j < i && k < i)
+                            continue;
+                        var testJPosKPos = new float2(position.x + j, position.y + k);
+                        if (grid.Physical[grid.ToGridPos(testJPosKPos)].Flags == flag)
+                        {
+                            return testJPosKPos;
+                        }
+                    
+                        var testJPosKNeg = new float2(position.x + j, position.y - k);
+                        if (grid.Physical[grid.ToGridPos(testJPosKNeg)].Flags == flag)
+                        {
+                            return testJPosKNeg;
+                        }
+                    
+                        var testJNegKPos = new float2(position.x - j, position.y + k);
+                        if (grid.Physical[grid.ToGridPos(testJNegKPos)].Flags == flag)
+                        {
+                            return testJNegKPos;
+                        }
+                    
+                        var testJNegKNeg = new float2(position.x - j, position.y - k);
+                        if (grid.Physical[grid.ToGridPos(testJNegKNeg)].Flags == flag)
+                        {
+                            return testJNegKNeg;
+                        }
+                    }
+                }
+            }
+
+            return position;
+        }
+        
+        public void Execute(ref FromTo fromTo)
+        {
+            if (fromTo.RelativeTo == Entity.Null)
+                return;
+            var position = Position2DFromEntity[fromTo.RelativeTo];
+
+            var nearestFire = FindNearestOfTag(position.Value, Grid.Cell.ContentFlags.Fire, Grid, GameMaster);
+            var nearestWater = FindNearestOfTag(nearestFire, Grid.Cell.ContentFlags.Water, Grid, GameMaster);
+
             if (math.any(fromTo.Source != nearestWater))
                 fromTo.Source = nearestWater;
             if (math.any(fromTo.Target != nearestFire))
                 fromTo.Target = nearestFire;
-        }).WithoutBurst().Run();
-    }
-
-    float2 FindNearestOfTag(float2 position, Grid.Cell.ContentFlags flag)
-    {
-        var grid = GetSingleton<Grid>();
-        var gameMaster = GetSingleton<GameMaster>();
-
-        var biggerLength = gameMaster.NbRows > gameMaster.NbCols ? gameMaster.NbRows : gameMaster.NbCols;
-
-        for (var i = 0; i < biggerLength; ++i)
-        {
-            for (var j = 0; j <= i; ++j)
-            {
-                for (var k = 0; k <= i; ++k)
-                {
-                    var testJPosKPos = new float2(position.x + j, position.y + k);
-                    if (grid.Physical[grid.ToGridPos(testJPosKPos)].Flags.HasFlag(flag))
-                    {
-                        return testJPosKPos;
-                    }
-                    
-                    var testJPosKNeg = new float2(position.x + j, position.y - k);
-                    if (grid.Physical[grid.ToGridPos(testJPosKNeg)].Flags.HasFlag(flag))
-                    {
-                        return testJPosKNeg;
-                    }
-                    
-                    var testJNegKPos = new float2(position.x - j, position.y + k);
-                    if (grid.Physical[grid.ToGridPos(testJNegKPos)].Flags.HasFlag(flag))
-                    {
-                        return testJNegKPos;
-                    }
-                    
-                    var testJNegKNeg = new float2(position.x - j, position.y - k);
-                    if (grid.Physical[grid.ToGridPos(testJNegKNeg)].Flags.HasFlag(flag))
-                    {
-                        return testJNegKNeg;
-                    }
-                    
-                }
-            }
         }
-
-        return position;
     }
     
-    
+    protected override JobHandle OnUpdate(JobHandle inputDeps)
+    {
+        var fromToJob = new FromToJob
+        {
+            Grid = GetSingleton<Grid>(),
+            GameMaster = GetSingleton<GameMaster>(),
+            Position2DFromEntity = GetComponentDataFromEntity<Position2D>(true)
+        };
+        
+        var fromToJobHandle = fromToJob.Schedule(this, inputDeps);
+
+        return fromToJobHandle;
+    }
 }
