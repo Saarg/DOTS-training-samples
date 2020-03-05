951b6a60b2e1b7816956e25625c472dd34be49bb
tim <tim@tim.tim>
2020-03-05 12:54:09 -0500

    Add an untested and purely speculative piece of mathematic

--- Ported/DOTS_BucketBrigade/Assets/Scripts/Components/DirectionComponent.cs
+++ Ported/DOTS_BucketBrigade/Assets/Scripts/Components/DirectionComponent.cs
@@ -1,7 +1,22 @@
 ﻿using Unity.Entities;
+using Unity.Entities.UniversalDelegates;
 using Unity.Mathematics;
 
 public struct Direction2D : IComponentData
 {
     public float2 Value;
+    
+    float3x3 ToRotationMatrix()
+    {
+        var d = Value;
+        return new float3x3
+            (
+                // t
+                new float3(d.y, 0, -d.x),
+                // b
+                new float3(0, 1, 0),
+                // n
+                new float3(d.x, 0, d.y)
+            );
+    }
 }
